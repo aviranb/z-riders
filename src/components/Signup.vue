81860ce46d79e23bb9b22291b93c8c387758ec27
aviranb <aviranb@jfrog.com>
2018-06-21 00:43:01 +0300

    submit signup form

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -3,14 +3,14 @@
         <div> Welcome to z-riders  </div>
         <div> Please enter your personal data </div>
         
-        <form @submit.prevent="validateBeforeSubmit">
+        <form v-on:submit.prevent="validateBeforeSubmit">
             <div class="form-field">
                 <label class="form-label">First name</label>
                 <div class="input-wrapper">
                     <input name="first-name" 
-                            v-model="firstName" 
+                            v-model="form.firstName" 
                             v-validate="'required'" 
-                            :class="{'input': true, 'danger': errors.has('first-name') }" 
+                            v-bind:class="{'input': true, 'danger': errors.has('first-name') }" 
                             type="text" 
                             placeholder="first name">
                     <span v-show="errors.has('first-name')" class="error-message">{{ errors.first('first-name') }}</span>
@@ -20,9 +20,9 @@
                 <label class="form-label">Last name</label>
                 <div class="input-wrapper">
                     <input name="last-name" 
-                    v-model="lastName" 
+                    v-model="form.lastName" 
                     v-validate="'required'" 
-                    :class="{'input': true, 'danger': errors.has('last-name') }" 
+                    v-bind:class="{'input': true, 'danger': errors.has('last-name') }" 
                     type="text"
                      placeholder="last name">
                     <span v-show="errors.has('last-name')" class="error-message">{{ errors.first('last-name') }}</span>
@@ -32,16 +32,16 @@
                 <label class="form-label">Id</label>
                 <div class="input-wrapper">
                     <input name="id" 
-                    v-model="id" 
+                    v-model="form.id" 
                     v-validate="'required|numeric|min:9|max:10'" 
-                    :class="{'input': true, 'danger': errors.has('id') }" 
+                    v-bind:class="{'input': true, 'danger': errors.has('id') }" 
                     type="text" placeholder="id">
                     <span v-show="errors.has('id')" class="error-message">{{ errors.first('id') }}</span>
                 </div>
             </div>
             <div class="form-field">
                  <p class="control">
-                <button class="button is-primary" type="submit">Submit</button>
+                <button class="button is-primary" type="submit" >Submit</button>
                 </p>
             </div>
         </form>
@@ -49,39 +49,44 @@
 </template>
 <script>
 //import users from '@/components/HelloWorld.vue'
-let db = require("./../config/firebase-config");
-let x = db.db
-  .ref("/users-test/" + 300826500)
-  .once("value")
-  .then(snapshot => {
-    console.log(snapshot.val());
-  });
-console.log(x);
+import Firebase from "firebase";
+let config = require("./../config/firebase-config");
+
 export default {
   data() {
     return {
-      firstName: "",
-      lastName:"",
-      id:""
+      form: {}
     };
   },
   methods: {
     signUp: function() {
-      console.log("ff", vm);
+      console.log("ff", this.form);
+      config.db
+        .ref("/users-test/")
+        .child(this.form.id)
+        .once("value", snapshot => {
+          let exists = snapshot.val() !== null;
+          this.userExistsCallback(this.form.id, exists);
+        });
     },
+
+    userExistsCallback(userId, exists) {
+      exists ? alert(userId + " exists") : alert(userId + " not exist");
+    },
+
     validateBeforeSubmit() {
       this.$validator.validateAll().then(result => {
         if (result) {
-          alert("Form Submitted!");
+          this.signUp();
           return;
         }
-
-        alert("Correct them errors!");
       });
     }
   }
 };
 </script>
+
+
 <style scoped>
 .container {
   flex: 1;
@@ -91,22 +96,21 @@ export default {
   flex-direction: column;
 }
 
-form{
-    padding: 20px;
-
+form {
+  padding: 20px;
 }
 
-.form-field{
-    display: flex;
-    flex-direction: column;
-    
-    height: 90px;
+.form-field {
+  display: flex;
+  flex-direction: column;
+
+  height: 90px;
 }
-.form-field .form-label{
-    display: flex;
-    align-items: flex-start;
-    justify-content: flex-start;
-    width: 300px;
+.form-field .form-label {
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  width: 300px;
 }
 
 input {
@@ -115,29 +119,27 @@ input {
   border: 1px solid #eee;
   border-left: 3px solid;
   border-radius: 5px;
-  transition: border-color .5s ease-out
+  transition: border-color 0.5s ease-out;
 }
 
-input.input.danger{
-   border-left-color:salmon;
+input.input.danger {
+  border-left-color: salmon;
 }
 
-input.input{
-   border-left-color:#eee
+input.input {
+  border-left-color: #eee;
 }
 
-input:focus{
-    outline: none;
+input:focus {
+  outline: none;
 }
 
-.input-wrapper{
-    display: flex;
-    flex-direction: column;
+.input-wrapper {
+  display: flex;
+  flex-direction: column;
 }
-.error-message{
-    color:salmon;
-    text-align: left;
+.error-message {
+  color: salmon;
+  text-align: left;
 }
-
-
 </style>
